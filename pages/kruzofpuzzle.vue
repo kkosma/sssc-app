59f9e8f0f8f37769fbad7c9b095484edc5f7d2ba
kkosma <kylekosma@Kyles-iMac.domain>
2018-06-12 19:20:09 -0700

    fix: plug memory leak by properly disposing three js scene

--- pages/kruzofpuzzle.vue
+++ pages/kruzofpuzzle.vue
@@ -37,7 +37,7 @@
       <div class="tutorial-popup-inner">
         <div class="tutorial-content">
           <span class="subhead"></span>
-          <p >Congragulations! <br> You've assembled the bones and Kruzof swims again.</p>
+          <p >Congratulations! <br> You've assembled the bones and Kruzof swims again.</p>
         </div>
         
         <div  class="btn btn-left" >Stay here</div>
@@ -82,7 +82,7 @@
           </div>
         </div>
       </div>
-    <script type="text/javascript" src="../js/kruzof.js"></script>
+   
   </div>
 </template>
 
@@ -228,12 +228,12 @@ export default {
       boneColors: { white: "#fff", blue: "#39B4FF",blueEmissive: "#003760" }
     };
   },
-  plugins: [{ src: "../js/kruzof.js", ssr: false }],
+  //plugins: [{ src: "../js/kruzof.js", ssr: false, body:true }],
   head() {
     return {
       title: this.name,
       script: [
-      
+        { src: "../js/kruzof.js", ssr: false, body:true }
       ]
     };
   },
@@ -244,7 +244,7 @@ export default {
       easing: "easeOutBack",
       calls: [[{ opacity: [1, 0], translateY: [0, 50] }]]
     });
-    var DEBUG = true;
+    var DEBUG = false;
     this.environment == 'production' ? DEBUG = false : null
     this.buildEnv == 'electron' ? DEBUG = false : null
     if (!DEBUG) {
@@ -273,7 +273,90 @@ export default {
       mode:'out-in',
       css:true,
       beforeLeave: function(el){
-        console.log('kruzofbefreleave')
+        //console.log('kruzofbefreleave')
+        //console.log(app.timers,'BeforTimers')
+        //console.log(app.callbackOnLoad,'LOADCALL')
+       // console.log(app.callbackOnLoad,'LOADCALL')
+        console.log(app.scene,"SCENE")
+       function disposeNode(parentObject) {
+          parentObject.traverse(function (node) {
+            
+              if (node instanceof THREE.Mesh) {
+                  if (node.geometry) {
+                    
+                      node.geometry.dispose();
+                     // node.geometry = undefined;
+                  }
+                  if (node.material) {
+                    
+                      var materialArray;
+                      if (node.material instanceof THREE.MeshFaceMaterial || node.material instanceof THREE.MultiMaterial) {
+                          materialArray = node.material.materials;
+                      }
+                      else if(node.material instanceof Array) {
+                          materialArray = node.material;
+                      }
+                      if(materialArray) {
+                          materialArray.forEach(function (mtrl, idx) {
+                            console.log(mtrl,"MAT")
+                              if (mtrl.map) mtrl.map.dispose();
+                              if (mtrl.lightMap) mtrl.lightMap.dispose();
+                              if (mtrl.bumpMap) mtrl.bumpMap.dispose();
+                              if (mtrl.normalMap) mtrl.normalMap.dispose();
+                              if (mtrl.specularMap) mtrl.specularMap.dispose();
+                              if (mtrl.envMap) mtrl.envMap.dispose();
+                              mtrl.dispose();
+                             // mrtl = undefined;
+                          });
+                      }
+                      else {
+                        console.log(node.material,"MAT")
+                          if (node.material.map) node.material.map.dispose();
+                          if (node.material.lightMap) node.material.lightMap.dispose();
+                          if (node.material.bumpMap) node.material.bumpMap.dispose();
+                          if (node.material.normalMap) node.material.normalMap.dispose();
+                          if (node.material.specularMap) node.material.specularMap.dispose();
+                          if (node.material.envMap) node.material.envMap.dispose();
+                          node.material.dispose();
+                         // node.material = undefined;
+                      }
+                  }
+                  
+              }
+              //app.scene.remove( node );
+          });
+      }
+      console.log(app.boneMatBump,"before Bone")
+      app.boneMatBump.dispose()
+       console.log(app.boneMatBump,"after Bone")
+      disposeNode(app.scene)
+      for (var i = app.scene.children.length - 1; i >= 0; i--) {
+        var child = app.scene.children[i];
+        console.log(child,'child')
+        app.scene.remove(child)
+      }
+      
+      console.log(app.scene,"SCENE DISPOSED")
+     // app.scene.dispose()
+      console.log(app.renderer.info,"RENDER INFO",app.scene)
+      app.renderer.renderLists.dispose();
+      app.renderer.dispose()
+      app.renderer.forceContextLoss(); 
+      app.renderer.context=undefined;
+      app.renderer.domElement=undefined;
+      app.renderer=null
+       
+       cancelAnimationFrame(app.renderRequest);
+        for (var timer in app.timers) {
+        //  console.log(timer,'LOOPTIMER')
+          clearTimeout(app.timers[timer])
+          //delete app.timers[timer]
+        }
+       app.timers={}
+     
+      // app={}
+       // console.log(app.timers,'HOMETIMERS')
+        
       },
       leave: function(el){
         console.log('kruzofleave')
